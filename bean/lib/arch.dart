5fe44bf6c008ba3320adbc429ebac170555f3383
BaselAllam <baseljahen@gmail.com>
2021-08-06 04:59:28 +0200

    commit

---
+++ bean/lib/arch.dart
@@ -0,0 +1,55 @@
+// Models: 
+// 	1- Category
+// 	2- Meal
+// 	3- User
+// 	4- Chief
+
+
+
+
+// -----------------------------
+
+// Func:
+// 	1- Cateogry
+// 		- get category
+
+// 	2- Meal
+// 		- get meals filter
+// 		- add meal
+// 		- update meal
+// 		- delete meal
+
+// 	3- User
+// 		- login
+// 		- register
+// 		- forgot passowrd
+// 		- logout
+// 		- update data
+// 		- add to fav
+// 		- remove from fav
+// 		- add review
+// 		- get reviews filter
+
+// 	4- Cheif
+// 		- login 
+// 		- register
+// 		- forgot password
+// 		- logout
+// 		- update data
+
+
+
+// -----------------------------
+
+// Screens:
+
+// 	1- Splash screen
+// 	2- Home ( Cateogries, Deals )
+// 	3- Result
+// 	4- Chief profile
+// 	5- User profile
+// 	6- Settings
+// 	7- meal details
+// 	8- wishlist
+// 	9- login
+// 	10- signup
